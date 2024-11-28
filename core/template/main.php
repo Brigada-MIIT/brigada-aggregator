e357213f6ba41dbc7876b4af4f9eccfb417c87c1
nikita <otghikogh@ya.ru>
2024-11-29 00:24:00 +0300

    update

--- core/template/main.php
+++ core/template/main.php
@@ -52,3 +52,26 @@
 
     </div>
 </main>
+
+<footer class="bg-light text-center text-lg-start mt-4">
+    <div class="container p-4">
+        <div class="row">
+            <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
+                <h5 class="text-uppercase">Ссылки</h5>
+                <ul class="list-unstyled">
+                    <li><a href="Team.html" class="text-primary">Наша команда</a></li>
+                </ul>
+            </div>
+            <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
+                <h5 class="text-uppercase">Социальные сети</h5>
+                <ul class="list-unstyled">
+                    <li><a href="https://web.telegram.org/k/#-4047480590" class="text-primary"><i class="fab fa-telegram"></i> Telegram</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+        © 2024 Copyright:
+        <a class="text-dark" href="#">Бригада</a>
+    </div>
+</footer>
