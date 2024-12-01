195be54fb74eb3e9cacc3ee26f2e93283231dd4f
nikita <otghikogh@ya.ru>
2024-12-01 15:49:33 +0300

    update

--- core/template/default.php
+++ core/template/default.php
@@ -8,6 +8,7 @@
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
     <link rel="stylesheet" href="/assets/css/front/main.css">
+    <link rel="stylesheet" href="/assets/css/dashboard.css">
     <script src="/assets/js/jquery.min.js"></script>
     <script src="/assets/js/sweetalert2.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
