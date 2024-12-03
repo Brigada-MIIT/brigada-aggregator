efdd88b1005693ec838de27898f87e31d1ea650a
nikita <otghikogh@ya.ru>
2024-12-04 01:38:28 +0300

    update

--- core/template/main.php
+++ core/template/main.php
@@ -5,55 +5,14 @@
         <button class="btn btn-primary search-button">Найти</button>
     </div>
 
-    <h2 class="popular-products-title mt-5">Популярные товары</h2>
-    <div class="product-categories row">
-        <div class="col-md-3 mb-4">
-            <div class="product-card">
-                <img src="/assets/img/front/metal.png" alt="Рельсы" class="img-fluid">
-                <h2>Рельсы</h2>
-                <button class="btn btn-primary">Узнать цену</button>
-            </div>
-        </div>
-        <div class="col-md-3 mb-4">
-            <div class="product-card">
-                <img src="/assets/img/front/stone.png" alt="Кирпич" class="img-fluid">
-                <h2>Кирпич</h2>
-                <button class="btn btn-primary">Узнать цену</button>
-            </div>
-        </div>
-        <div class="col-md-3 mb-4">
-            <div class="product-card">
-                <img src="/assets/img/front/wood.png" alt="Дерево" class="img-fluid">
-                <h2>Дерево</h2>
-                <button class="btn btn-primary">Узнать цену</button>
-            </div>
-        </div>
-        <div class="col-md-3 mb-4">
-            <div class="product-card">
-                <img src="/assets/img/front/other_services.jpg" alt="Прочие услуги" class="img-fluid">
-                <h2>Прочие услуги</h2>
-                <button class="btn btn-primary">Узнать цену</button>
-            </div>
-        </div>
-    </div>
 
-    <div class="product-selection mt-4">
-        <h2>Категории товаров</h2>
-        <div class="form-group">
-            <select class="form-control" id="productCategory">
-                <option value="">Выберите категорию</option>
-                <option value="electronics">Электроника</option>
-                <option value="kitchen">Кухонные товары</option>
-            </select>
-        </div>
-    </div>
 
     <div class="pagination justify-content-center mt-4">
 
     </div>
 </div>
 
-<footer class="bg-light text-center text-lg-start mt-4">
+<footer class="bg-light text-center text-lg-start mt-4 fixed-bottom">
     <div class="container p-4">
         <div class="row">
             <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
