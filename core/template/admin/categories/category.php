b929838935637e0a6022ae32f99b29d18f1cd085
nikita <otghikogh@ya.ru>
2024-12-06 16:00:50 +0300

    update

--- core/template/admin/categories/category.php
+++ core/template/admin/categories/category.php
@@ -21,7 +21,7 @@
         </div>
         <div class="col-12" style="margin-top: 5%;">
             <div class="in">
-                <div class="btn-group d-flex flex-wrap">
+                <div class="d-flex flex-wrap">
                     <button id="submit" type="submit" class="btn btn-primary mr-4 mb-2" onclick="edit();">Сохранить</button>
                     <button id="submit" type="submit" class="btn btn-danger mr-4 mb-2" onclick="submit_delete();">Удалить категорию</button>
                 </div>
