39567bba60d5f7a0a27bb40223e54970cd0e97d7
nikita <otghikogh@ya.ru>
2024-12-06 03:13:54 +0300

    update

--- core/handlers.php
+++ core/handlers.php
@@ -949,7 +949,7 @@ function api_subcategories_edit() {
         res(0);
     $id = !empty(intval($_REQUEST['id'])) ? intval($_REQUEST['id']) : res(0, "id error");
     $name = !empty($_REQUEST['name']) ? $_REQUEST['name'] : res(0, "Введите имя подкатегории");
-    $category_id = !empty(intval($_REQUEST['$category_id'])) ? intval($_REQUEST['$category_id']) : res(0, "Выберите категорию");
+    $category_id = !empty(intval($_REQUEST['category_id'])) ? intval($_REQUEST['category_id']) : res(0, "Выберите категорию");
     $description = !empty($_REQUEST['description']) ? $_REQUEST['description'] : res(0, "Введите описание подкатегории");
     $picture_url = !empty($_REQUEST['picture_url']) ? "'".$_REQUEST['picture_url']."'" : "NULL";
     $db = $system->db();
