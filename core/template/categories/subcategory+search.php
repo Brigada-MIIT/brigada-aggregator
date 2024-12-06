7109f39b9db2ad2c4275c38b8613cd654615c3b7
nikita <otghikogh@ya.ru>
2024-12-06 23:27:37 +0300

    update

--- core/template/categories/subcategory+search.php
+++ core/template/categories/subcategory+search.php
@@ -56,7 +56,7 @@
                         <div class='modal-dialog modal-dialog-centered' role='document'>
                             <div class='modal-content'>
                                 <div class='modal-header'>
-                                    <h5 class='modal-title' id='comparisonModalLabel".($i+1)."'>Сравнение цен для ${name}</h5>
+                                    <h5 class='modal-title' id='comparisonModalLabel".($i+1)."'>Сравнение цен для «${name}»</h5>
                                     <button type='button' class='close' data-dismiss='modal' aria-label='Close'>
                                         <span aria-hidden='true'>×</span>
                                     </button>
